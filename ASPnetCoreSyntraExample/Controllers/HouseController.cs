984e3c2f6caff83f747c57d88c98d21eeee2dcf4
Maxim Crabbé <maxim.crabbe@craxit.be>
2021-02-23 13:09:21 +0100

    real init, asp.net web api

---
+++ ASPnetCoreSyntraExample/Controllers/HouseController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPnetCoreSyntraExample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HouseController : ControllerBase
+    {
+        static List<House> houses = new List<House>();
+        public HouseController()
+        {
+            if (houses.Count == 0)
+            {
+                var house1 = new House();
+                house1.Name = "H1";
+                house1.Area = "100m2";
+                house1.Price = 100000;
+                houses.Add(house1);
+            }
+        }
+        [HttpGet("many")]
+        public List<House> GetAllHouses()
+        {
+            return houses;
+        }
+        [HttpGet("one")]
+        public ActionResult<House> GetHouse(string houseName)
+        {
+            var house = houses.First(x => x.Name == houseName);
+            return Ok(house);
+        }
+        [HttpPost]
+        public void CreateNewHouse(House newHouse)
+        {
+            houses.Add(newHouse);
+        }
+        [HttpDelete]
+        public void DeleteHouse(string houseName)
+        {
+            var houseToDelete = houses.First(x => x.Name == houseName);
+            houses.Remove(houseToDelete);
+        }
+
+        [HttpPut]
+        public House UpdateHouseByName(string nameOfHouseToEdit, House houseEditValues)
+        {
+            var houseToEdit = houses.First(x => x.Name == nameOfHouseToEdit);
+            houseToEdit.Price = houseEditValues.Price;
+            houseToEdit.Area = houseEditValues.Area;
+            return houseToEdit;
+
+
+        }
+
+    }
+    public class House
+    {
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+    }
+}
---
+++ ASPnetCoreSyntraExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPnetCoreSyntraExample.Controllers
+{
+    [ApiController]
+    [Route("hetweerbericht")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    var returnval =  Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //    return returnval;
+        //}
+
+        [HttpPost]
+        public int Get()
+        {
+            var rng = new Random();
+            var hetgetal = rng.Next(0, 100);
+            return hetgetal;
+        }
+    }
+}
